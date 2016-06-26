3c20633f776f5667ae2d85955c7ea9c3d90c2e11
Alexander C Rees <me@acrees.co.uk>
2016-06-26 17:06:25 +0100

    Can't overwrite file with jq; put somewhere else

--- deploy-marathon.sh
+++ deploy-marathon.sh
@@ -1,9 +1,11 @@
 APP_ID=react-graphs
 TAG=acrees/react-graphs:$(git rev-parse --short HEAD)
 
-cat marathon.json | \
-  jq ".container.docker.image |= \"$TAG\"" > marathon.json
+mkdir out
 
-curl -sSfX PUT "$MARATHON_HOST/v2/$MARATHON_JSON_TYPE/$APP_ID" -d "@marathon.json" \
+cat marathon.json | \
+  jq ".container.docker.image |= \"$TAG\"" > out/marathon.json
+
+  curl -sSfX PUT "$MARATHON_HOST/v2/$MARATHON_JSON_TYPE/$APP_ID" -d "@out/marathon.json" \
   -H "Content-type: application/json" \
   --header "Authorization: token=$DCOS_TOKEN"
