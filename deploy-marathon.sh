ccadb493d5845626ffe6aa13ec3364f5c2b8d073
Alexander C Rees <me@acrees.co.uk>
2016-06-26 16:37:36 +0100

    Tagging images with commit hash

--- deploy-marathon.sh
+++ deploy-marathon.sh
@@ -1,4 +1,9 @@
 APP_ID=react-graphs
-curl -X PUT "$MARATHON_HOST/v2/$MARATHON_JSON_TYPE/$APP_ID" -d "@marathon.json" \
+TAG=acrees/react-graphs:$(git rev-parse --short HEAD)
+
+cat marathon.json | \
+  jq ".container.docker.image |= \"$TAG\"" > marathon.json
+
+curl -sSfX PUT "$MARATHON_HOST/v2/$MARATHON_JSON_TYPE/$APP_ID" -d "@marathon.json" \
   -H "Content-type: application/json" \
   --header "Authorization: token=$DCOS_TOKEN"
